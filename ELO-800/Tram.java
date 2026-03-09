46caf9c2334ec16992aff14f269ef5276d34d77b
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-09 21:03:26 +0800

    Init Tram

---
+++ ELO-800/Tram.java
@@ -0,0 +1,14 @@
+// 116A - Tram
+
+import java.util.Scanner;
+
+public class Tram{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+
+        s.close();
+
+    }
+}
