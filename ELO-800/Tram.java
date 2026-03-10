5cc7f73a9f09c1f1e3a5215e7f050c5aa283f7ff
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-11 05:13:59 +0800

    Tram (Initial)

--- ELO-800/Tram.java
+++ ELO-800/Tram.java
@@ -7,6 +7,22 @@ public class Tram{
 
         Scanner s = new Scanner(System.in);
 
+        int max_cap = 0;
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++) {
+
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            int exit = b - a;
+
+            if(i == 0) continue;
+
+            max_cap = exit + b;
+        }
+
+        System.out.println(max_cap);
 
         s.close();
 
