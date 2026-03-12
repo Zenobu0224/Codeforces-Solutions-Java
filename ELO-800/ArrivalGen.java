cee989948a8d311aa9ba4c4496901dec3c5d21e9
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-12 23:11:09 +0800

    Arrival of the General Not Yet Complete

--- ELO-800/ArrivalGen.java
+++ ELO-800/ArrivalGen.java
@@ -3,9 +3,46 @@
 import java.util.Scanner;
 
 public class ArrivalGen {
+
+    static int[] heightFL(int[] soldier_height) {
+
+        int lowest = Integer.MAX_VALUE;
+        int highest = Integer.MIN_VALUE;
+
+        int steps = 0;
+
+
+        for(int i = 0; i < soldier_height.length; i++) {
+
+            if(lowest <= soldier_height[i]) lowest = i;
+
+        }
+
+        for(int i = lowest+1; i < soldier_height.length - (lowest+1); i++) {
+            int lowest_i = i;
+            if(soldier_height[i] < soldier_height[lowest]){
+    
+                soldier_height[]
+
+            }
+        }
+
+
+
+    }
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int[] soldier_height = new int[n];
+
+        for(int i = 0; i < n; i++) {
+
+            int a = s.nextInt();
+            soldier_height[i] = a;
+
+        }
 
         s.close();
 
