b22a04b683a28fb1dc7914ff653c84041889d8a7
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-03-12 22:48:07 +0800

    144A - Arrival of General Init

---
+++ ELO-800/ArrivalGen.java
@@ -0,0 +1,13 @@
+// 144A - Arrival of the General
+
+import java.util.Scanner;
+
+public class ArrivalGen {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        s.close();
+
+    }
+}
